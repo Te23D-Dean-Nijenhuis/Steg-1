051f11a875b51df1772b0ebf856d21df44787290
Dean Nijenhuis <Dean.Nijenhuis@elev.ga.ntig.se>
2024-11-11 08:55:33 +0100

    gabagouh

---
+++ Steg 1/Program.cs
@@ -0,0 +1,23 @@
+﻿// if (6 >= 3)
+// {
+//     Console.WriteLine("hello world");
+// }
+
+using System.Xml;
+
+Console.WriteLine("skriv ditt namn");
+string name = Console.ReadLine();
+Console.WriteLine("skriv ditt lösenord");
+string password = Console.ReadLine();
+
+if (name.ToLower() == "kalleanka" && password.ToLower() == "12345")
+{
+    Console.WriteLine("hola");
+} else
+{
+    Console.WriteLine("antingen så stämmer varken lösenorde eller användarnamn");
+}
+
+
+
+Console.ReadLine();
