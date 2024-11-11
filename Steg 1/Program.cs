fa9023aea47f68fdb43f352044b4c7c7bd027eef
Dean Nijenhuis <Dean.Nijenhuis@elev.ga.ntig.se>
2024-11-11 08:59:11 +0100

    gabbba gouu

--- Steg 1/Program.cs
+++ Steg 1/Program.cs
@@ -3,21 +3,26 @@
 //     Console.WriteLine("hello world");
 // }
 
-using System.Xml;
 
-Console.WriteLine("skriv ditt namn");
-string name = Console.ReadLine();
-Console.WriteLine("skriv ditt lösenord");
-string password = Console.ReadLine();
+// Console.WriteLine("skriv ditt namn");
+// string name = Console.ReadLine();
+// Console.WriteLine("skriv ditt lösenord");
+// string password = Console.ReadLine();
 
-if (name.ToLower() == "kalleanka" && password.ToLower() == "12345")
+// if (name.ToLower() == "kalleanka" && password.ToLower() == "12345")
+// {
+//     Console.WriteLine("hola");
+// } else
+// {
+//     Console.WriteLine("antingen så stämmer varken lösenord eller användarnamn");
+// }
+
+for (int i = 0; i < 32; i++)
 {
-    Console.WriteLine("hola");
-} else
-{
-    Console.WriteLine("antingen så stämmer varken lösenorde eller användarnamn");
+    Console.WriteLine("hej världen");
 }
 
 
 
 Console.ReadLine();
+
